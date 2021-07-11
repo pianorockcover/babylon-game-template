f56cc82b9fe7c31ff81485eb86264b27c56a86ce
Your Name <you@example.com>
2021-07-11 10:11:58 -0700

    box grid system

---
+++ src/game/elements/index.ts
@@ -0,0 +1,14 @@
+export interface Coordinates {
+    x: number;
+    y: number;
+    z: number;
+}
+
+export interface RGBA {
+    r: number;
+    b: number;
+    g: number;
+    a: number;
+}
+
+export const boxScale = 1;
--- src/game/generateWorld.ts
+++ src/game/generateWorld.ts
@@ -1,5 +1,199 @@
 import { MeshBuilder, Scene } from "babylonjs";
+import { Box } from "./elements/Box";
 
 export const generateWorld = (scene: Scene): void => {
-  MeshBuilder.CreateBox("box", {}, scene);
+  new Box({
+    scene, size: "eighth", coordinates: {
+      x: 0,
+      y: 0,
+      z: 0
+    },
+    color: {
+      r: 27,
+      g: 202,
+      b: 65,
+      a: 1
+    },
+  });
+
+  new Box({
+    scene,
+    size: "eighth",
+    coordinates: {
+      x: 0,
+      y: 1,
+      z: 0
+    },
+    color: {
+      r: 27,
+      g: 202,
+      b: 65,
+      a: 1
+    }
+  });
+
+  new Box({
+    scene,
+    size: "eighth",
+    coordinates: {
+      x: 0,
+      y: 2,
+      z: 0
+    },
+    color: {
+      r: 27,
+      g: 202,
+      b: 65,
+      a: 1
+    }
+  });
+
+  // 
+
+  new Box({
+    scene,
+    size: "eighth",
+    coordinates: {
+      x: 0,
+      y: 3,
+      z: 0
+    },
+    color: {
+      r: 202,
+      g: 27,
+      b: 27,
+      a: 1
+    }
+  });
+
+  new Box({
+    scene,
+    size: "eighth",
+    coordinates: {
+      x: -1,
+      y: 3,
+      z: 0
+    },
+    color: {
+      r: 202,
+      g: 27,
+      b: 27,
+      a: 1
+    }
+  });
+
+  new Box({
+    scene,
+    size: "eighth",
+    coordinates: {
+      x: 1,
+      y: 3,
+      z: 0
+    },
+    color: {
+      r: 202,
+      g: 27,
+      b: 27,
+      a: 1
+    }
+  });
+
+  // 
+  new Box({
+    scene,
+    size: "eighth",
+    coordinates: {
+      x: 0,
+      y: 4,
+      z: 0
+    },
+    color: {
+      r: 199,
+      g: 202,
+      b: 27,
+      a: 1
+    }
+  });
+
+  new Box({
+    scene,
+    size: "eighth",
+    coordinates: {
+      x: -1,
+      y: 4,
+      z: 0
+    },
+    color: {
+      r: 202,
+      g: 27,
+      b: 27,
+      a: 1
+    }
+  });
+
+  new Box({
+    scene,
+    size: "eighth",
+    coordinates: {
+      x: 1,
+      y: 4,
+      z: 0
+    },
+    color: {
+      r: 202,
+      g: 27,
+      b: 27,
+      a: 1
+    }
+  });
+
+  // 
+
+  new Box({
+    scene,
+    size: "eighth",
+    coordinates: {
+      x: 0,
+      y: 5,
+      z: 0
+    },
+    color: {
+      r: 202,
+      g: 27,
+      b: 27,
+      a: 1
+    }
+  });
+
+  new Box({
+    scene,
+    size: "eighth",
+    coordinates: {
+      x: -1,
+      y: 5,
+      z: 0
+    },
+    color: {
+      r: 202,
+      g: 27,
+      b: 27,
+      a: 1
+    }
+  });
+
+  new Box({
+    scene,
+    size: "eighth",
+    coordinates: {
+      x: 1,
+      y: 5,
+      z: 0
+    },
+    color: {
+      r: 202,
+      g: 27,
+      b: 27,
+      a: 1
+    }
+  });
 };
